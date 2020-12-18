095fc304e2e126afda62cc1bc82cd3421110a38e
jimmy <jonssonjimmy8@gmail.com>
2020-12-17 22:05:17 -0500

    generate candidate model

--- db/migrate/20201218024747_create_candidates.rb
+++ db/migrate/20201218024747_create_candidates.rb
@@ -1,16 +1,15 @@
-class CreateUsers < ActiveRecord::Migration[6.0]
+class CreateCandidates < ActiveRecord::Migration[6.0]
   def change
-    create_table :users do |t|
+    create_table :candidates do |t|
       t.string :first_name
       t.string :last_name
       t.string :email
       t.integer :age
       t.string :gender
       t.string :party_registered_to
-      t.string :county
-      t.string :race
-      t.string :ethnicity
-      t.integer :post_id
+      t.integer :issues_id
+      t.integer :thumbs_up
+      t.integer :thumbs_down
 
       t.timestamps
     end
