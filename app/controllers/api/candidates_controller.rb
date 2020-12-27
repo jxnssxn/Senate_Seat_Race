c9c3749264a2151aab1ed7bb22bcbd14fb22457b
jimmy <jonssonjimmy8@gmail.com>
2020-12-26 23:20:13 -0500

    finished seeds file, Issues.create has bug. create Candidates index action/route and .json.jb file.

--- app/controllers/api/candidates_controller.rb
+++ app/controllers/api/candidates_controller.rb
@@ -1,2 +1,8 @@
 class Api::CandidatesController < ApplicationController
+
+
+  def index
+    @message = "candidate index"
+    render "index.json.jb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
-    get "/photos" => "photos#index"
+    
+    get "/candidates/" => "candidates#index"
+
   end
 end
