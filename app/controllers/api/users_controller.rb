ac7fc599e30b1d12d6d161be456b86e7f43b17bb
jimmy <jonssonjimmy8@gmail.com>
2020-12-27 02:35:37 -0500

    updated users index action dynamic.

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,6 +1,7 @@
 class Api::UsersController < ApplicationController
   def index
-    @message = "user index action"
+    # @message = "user index action"
+    @users = User.all
     render "index.json.jb"
   end
 end
