85a6ecd2c8caf8581b5ca38c1e3c81be8c5ebc81
jimmy <jonssonjimmy8@gmail.com>
2021-01-27 21:33:46 -0500

    set root as route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # EXAMPLE HTML ROUTE
   # get "/photos" => "photos#index"
-
+  root 'candidates#index'
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     
